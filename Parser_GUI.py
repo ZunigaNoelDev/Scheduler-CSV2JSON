7c52ddf4407d43fe62ef513b9121b14c9d6dc575
ZunigaNoelDev <zuniganoeldev@gmail.com>
2022-03-23 11:51:15 -0700

    Added final steps for data entry

--- Parser_GUI.py
+++ Parser_GUI.py
@@ -19,18 +19,24 @@ def InitializeRoot(title, width_ratio, height_ratio, x_pos=0, y_pos=0, columns=1
 		root.columnconfigure(i, weight=1)
 	return root
 
-def NewButton(parent, key, labelText, buttonText, method, row, col, pad_x, pad_y, row_offset=0, col_offset=1):
+def NewButton(parent, key, labelText, buttonText, method, row, col, pad_x, pad_y, pad_x_2=0, row_offset=0, col_offset=1, hasLabel=True, sticky="w"):
 	widgets[key] = {"label": Label(parent, text=labelText), "button": Button(parent, text=buttonText, command=method)}
 
-	widgets[key]["label"].grid(row=row, column=col, 
-		sticky="w", columnspan=2,
-		padx=(parent.winfo_width()*pad_x,0),
-		pady=(parent.winfo_width()*pad_y,0))
+	if hasLabel:
+		widgets[key]["label"].grid(row=row, column=col, 
+			sticky="w", columnspan=2,
+			padx=(parent.winfo_width()*pad_x,0),
+			pady=(parent.winfo_width()*pad_y,0))
 
-	widgets[key]["button"].grid(row=row+row_offset, column=col+col_offset, 
-		sticky="e", columnspan=2,
-		padx=(0,parent.winfo_width()*pad_x),
-		pady=(parent.winfo_width()*pad_y,0))
+		widgets[key]["button"].grid(row=row+row_offset, column=col+col_offset, 
+			sticky="e", columnspan=2,
+			padx=(0,parent.winfo_width()*pad_x),
+			pady=(parent.winfo_width()*pad_y,0))
+	else:
+		widgets[key]["button"].grid(row=row+row_offset, column=col+col_offset, 
+			sticky=sticky, columnspan=1,
+			padx=(parent.winfo_width()*pad_x_2, parent.winfo_width()*pad_x),
+			pady=(parent.winfo_width()*pad_y,0))
 
 	return widgets[key]
 
@@ -80,8 +86,7 @@ def NewOptionMenu(parent, key, labelText, optionList, curr_selection, buttonText
 	return widgets[key]
 
 def StepOne():
-	# widgets["Location"]["button"]["text"] = "Update"
-	path = os.getcwd() + "\\Files"
+	path = f"{os.getcwd()}{os.path.sep}Files{os.path.sep}"
 	if not "Courses" in widgets.keys():
 		courses_widget = NewButton(
 			parent=root,
@@ -92,15 +97,17 @@ def StepOne():
 			pad_x=0.05, pad_y=0.05,
 			row=1, col=0)
 	widgets["Courses"]["label"].focus()
-	if os.path.exists(path+"\\courses.json"):
-		StepTwo(path+"\\courses.json")
+	if os.path.exists(path+"courses.json"):
+		files["Courses"] = path+"courses.json"
+		StepTwo(path+"courses.json")
 
 
 def StepTwo(filepath):
-	path = os.getcwd() + "\\Files"
-	filename = filepath.split("\\")[-1]
+	path = f"{os.getcwd()}{os.path.sep}Files{os.path.sep}"
+	filename = filepath.split(os.path.sep)[-1]
 	widgets["Courses"]["label"]["text"] = f"1.) {filename}"
 	widgets["Courses"]["button"]["text"] = "Change"
+	courses = GetJVals(filepath, "name")
 
 	if not "Locations" in widgets.keys():
 		locations_widget = NewButton(
@@ -108,42 +115,43 @@ def StepTwo(filepath):
 			key="Locations",
 			labelText=f"2.) Locations",
 			buttonText="Load",
-			method=lambda:LoadFile("Open a JSON File", os.getcwd() + "/Files/", (('JSON files', '*.json'),), StepThree, key="Locations"),
+			method=lambda:LoadFile("Open a JSON File", path, (('JSON files', '*.json'),), StepThree, key="Locations"),
 			pad_x=0.05, pad_y=0.05,
 			row=2, col=0)
-	if os.path.exists(path+"\\locations.json"):
-		StepThree(path+"\\locations.json")
+	if os.path.exists(path+"locations.json"):
+		files["Locations"] = path+"locations.json"
+		StepThree(path+"locations.json")
 
 
 def StepThree(filepath):
-	path = os.getcwd() + "\\Files"
-	filename = filepath.split("\\")[-1]
+	path = f"{os.getcwd()}{os.path.sep}Files{os.path.sep}"
+	filename = filepath.split(os.path.sep)[-1]
 	widgets["Locations"]["label"]["text"] = f"2.) {filename}"
 	widgets["Locations"]["button"]["text"] = "Change"
-	locations = GetJKeys(filepath)
+	locations = GetJVals(filepath, "name")
 
 	CSV_Widget = NewButton(
 		parent=root,
 		key="CSV",
 		labelText="3.) CSV File",
 		buttonText="Load",
-		method=lambda:LoadFile("Open a CSV File", os.getcwd() + "/Files/", (('CSV files', '*.csv'),), StepFour, key="Schedule"),
+		method=lambda:LoadFile("Open a CSV File", path, (('CSV files', '*.csv'),), StepFour, key="Schedule"),
 		pad_x=0.05, pad_y=0.05,
 		row=3, col=0)
 	loc = None
 	for location in locations:
-		if os.path.exists(path+f"\\{location}.csv"):
+		if os.path.exists(path+f"{location}.csv"):
 			loc = location
-			files["Schedule"] = path+f"\\{location}.csv"
-			StepFour(path+f"\\{location}.csv", locations)
+			files["Schedule"] = path+f"{location}.csv"
+			StepFour(path+f"{location}.csv", locations)
 			break
-	if not loc and os.path.exists(path+f"\\schedule.csv"):
-		files["Schedule"] = path+"\\schedule.csv"
-		StepFour(path+f"\\schedule.csv", locations)
+	if not loc and os.path.exists(path+f"schedule.csv"):
+		files["Schedule"] = path+"schedule.csv"
+		StepFour(path+f"schedule.csv", locations)
 
 
 def StepFour(filepath, locations):
-	filename = filepath.split("\\")[-1]
+	filename = filepath.split(os.path.sep)[-1]
 	widgets["CSV"]["label"]["text"] = f"3.) {filename}"
 	widgets["CSV"]["button"]["text"] = "Change"
 
@@ -164,7 +172,7 @@ def StepFour(filepath, locations):
 			buttonText="Confirm",
 			method=StepFive,
 			pad_x=0.05, pad_y=0.05,
-			row=4, col=0, width=12)
+			row=4, col=0, width=7)
 	widgets["Location"]["options"].focus()
 
 
@@ -181,12 +189,6 @@ def StepFive():
 			method=StepSix,
 			pad_x=0.05, pad_y=0.05,
 			row=5, col=0)
-		
-		# names_col_l, names_col = NewEntry(root, "Column Containing Course Names", 4, 1)
-		# dates_first_l, dates_first = NewEntry(root, "Column Containing First Date", 5, 1)
-		# dates_last_l, dates_lase = NewEntry(root, "Column Containing Last Date", 6, 1)
-		# price__5D_col_l, price__5D_col = NewEntry(root, "Column Containing 5 Day Price", 7, 1)
-		# price__4D_col_l, price__4D_col = NewEntry(root, "Column Containing 4 Day Price", 8, 1)
 
 
 def StepSix():
@@ -195,7 +197,7 @@ def StepSix():
 		schedule_row_start = NewEntry(
 			parent=root,
 			key="Start Row",
-			labelText="6.) First Schedule Row",
+			labelText="6.) Grid Start Row",
 			entryText=[val if (val:=GetScheduleRows(files["Schedule"], "open")[0]+1) > 0 else -1][0],
 			entryWidth=len(str(val)) if val > 0 else 2,
 			buttonText="Confirm",
@@ -206,7 +208,7 @@ def StepSix():
 		schedule_row_end = NewEntry(
 			parent=root,
 			key="End Row",
-			labelText="      Last Schedule Row",
+			labelText="      Grid End Row",
 			entryText=[val if (val:=GetScheduleRows(files["Schedule"], "open")[1]+1) > 0 else -1][0],
 			entryWidth=len(str(val)) if val > 0 else 2,
 			buttonText="Confirm",
@@ -216,7 +218,82 @@ def StepSix():
 
 
 def StepSeven():
-	pass
+	widgets["End Row"]["button"]["text"] = "Change"
+	if not "Course Names" in widgets.keys():
+		course_names = NewEntry(
+			parent=root,
+			key="Course Names",
+			labelText="7.) Names Column",
+			entryText=(val:=GetCourseNamesCol(files["Schedule"], GetJVals(files["Courses"], "name"))),
+			entryWidth=len(val) if val != -1 else 2,
+			buttonText="Confirm",
+			method=StepEight,
+			pad_x=0.05, pad_y=0.05,
+			row=8, col=0)
+
+def StepEight():
+	widgets["Course Names"]["button"]["text"] = "Change"
+	if not "Start Col" in widgets.keys():
+		schedule_row_start = NewEntry(
+			parent=root,
+			key="Start Col",
+			labelText="8.) Grid Start Col",
+			entryText=(val:=GetDatesCols(files["Schedule"])[0]),
+			entryWidth=len(val) if val != -1 else 2,
+			buttonText="Confirm",
+			method=StepNine,
+			hasButton=False,
+			pad_x=0.05, pad_y=0.05,
+			row=9, col=0)
+		schedule_row_end = NewEntry(
+			parent=root,
+			key="End Col",
+			labelText="      Grid End Col",
+			entryText=(val:=GetDatesCols(files["Schedule"])[1]),
+			entryWidth=len(val) if val != -1 else 2,
+			buttonText="Confirm",
+			method=StepNine,
+			pad_x=0.05, pad_y=0.00,
+			row=10, col=0)
+
+def StepNine():
+	widgets["End Col"]["button"]["text"] = "Change"
+	if not "Price Col 5" in widgets.keys():
+		schedule_row_start = NewEntry(
+			parent=root,
+			key="Price Col 5",
+			labelText="9.) $ 5-Day Col",
+			entryText=(val:=GetPriceCol(files["Schedule"])[0]),
+			entryWidth=len(val) if val != -1 else 2,
+			buttonText="Confirm",
+			method=StepTen,
+			hasButton=False,
+			pad_x=0.05, pad_y=0.05,
+			row=11, col=0)
+		schedule_row_end = NewEntry(
+			parent=root,
+			key="Price Col 4",
+			labelText="      $ 4-Day Col",
+			entryText=(val:=GetPriceCol(files["Schedule"])[1]),
+			entryWidth=len(val) if val != -1 else 2,
+			buttonText="Confirm",
+			method=StepTen,
+			pad_x=0.05, pad_y=0.00,
+			row=12, col=0)
+
+def StepTen():
+	widgets["Price Col 4"]["button"]["text"] = "Change"
+	courses_widget = NewButton(
+		parent=root,
+		key="Preview",
+		labelText="",
+		buttonText="Preview",
+		method="",
+		pad_x=0.05, pad_y=0.05, pad_x_2=0.05,
+		row=13, col=0, hasLabel=False, col_offset=0)
+
+def Post():
+	print("Sending to server")
 
 
 def GetWeeksRow(filepath):
@@ -252,10 +329,47 @@ def GetScheduleRows(filepath, keyword):
 
 	return start_row, end_row
 
-def GetJKeys(filepath):
+
+def GetCourseNamesCol(filepath, courses):
+	start = int(widgets["Start Row"]["entry"].get())-1
+	end = int(widgets["End Row"]["entry"].get())-1
+	columns = []
+	with open(filepath, "r") as read_obj:
+		reader = csv.reader(read_obj)
+		for index, row in enumerate(reader):
+			if index >= start and index <= end:
+				for col_num, col in enumerate(row):
+					if col in courses:
+						columns.append(col_num)
+	if len(columns) > 0:
+		return chr(max(set(columns), key = columns.count)+65)
+	return -1
+
+
+def GetDatesCols(filepath):
+	weeks_row = int(widgets["Weeks Row"]["entry"].get())
+	first = True
+	start, end = -1, -1
+	with open(filepath, "r") as read_obj:
+		reader = csv.reader(read_obj)
+		for index, row in enumerate(reader):
+			if index == weeks_row-1:
+				for col_num, col in enumerate(row):
+					if col and "/" in col and first:
+						start = col_num
+						first = False
+					elif col and "/" in col:
+						end = col_num
+	return chr(start+65), chr(end+65)
+
+
+def GetPriceCol(filepath):
+	return -1,-1
+
+def GetJVals(filepath, key):
 	with open(filepath) as jsonFile:
 		data = json.load(jsonFile)
-		return [entry["name"] for entry in data]
+		return [entry[key] for entry in data]
 
 def LoadFile(title, dir, filetypes, nextstep, key=None):
 	filepath = fd.askopenfilename(
@@ -272,13 +386,16 @@ files = {}
 # Initializing root window with a custom method to simplify customization
 root = InitializeRoot(
 	title="Spreadsheet Schedule Parser",
-	width_ratio=.25,
-	height_ratio=.5,
-	x_pos=.70,
-	y_pos=.25,
+	width_ratio=.3,
+	height_ratio=.75,
+	x_pos=.65,
+	y_pos=.05,
 	columns=3)
 root.update_idletasks()
 
 widgets = {}
+locations = {}
+courses = {}
+
 StepOne()
 root.mainloop()
